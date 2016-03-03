7ab4f5f900de87bea34198785e57bc931803098b
Dave Harned <dharned@gmail.com>
2016-03-03 00:48:51 -0600

    activate button click event

--- js/main.js
+++ js/main.js
@@ -1,23 +1,26 @@
-// apiKey variable loaded via config.js file, set to .gitignore for security
+// 'appId' and 'apiKey' variables loaded via config.js file, set to .gitignore for security
 
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous", true);
-xhr.setRequestHeader("X-Parse-Application-Id", appId);
-xhr.setRequestHeader("X-Mashape-Key", apiKey);
-xhr.setRequestHeader("Content-Type", "application/json");
-xhr.send();
-xhr.onload = function() {
-	if (xhr.readyState === 4) {
-	console.log(xhr.status);
-	console.log(xhr.statusText);
-	var responseObject = JSON.parse(xhr.responseText);
-
-	var newQuote = responseObject.quote;
-	var newAuthor = responseObject.author;
-	var quoteField = document.getElementById('quote');
-	var authorField = document.getElementById('author');
-	quoteField.innerHTML = newQuote;
-	authorField.innerHTML = newAuthor;
-
-	}
+function newQuote() {
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous", true);
+	xhr.setRequestHeader("X-Parse-Application-Id", appId);
+	xhr.setRequestHeader("X-Mashape-Key", apiKey);
+	xhr.setRequestHeader("Content-Type", "application/json");
+	xhr.send();
+	xhr.onload = function() {
+		if (xhr.readyState === 4) {
+			var responseObject = JSON.parse(xhr.responseText);
+			var newQuote = responseObject.quote;
+			var newAuthor = responseObject.author;
+			var quoteField = document.getElementById('quote');
+			var authorField = document.getElementById('author');
+			quoteField.innerHTML = newQuote;
+			authorField.innerHTML = newAuthor;
+		}
+	}	
 }
+
+newQuote();
+
+var button = document.getElementById('button');
+button.addEventListener("click", newQuote, false);
